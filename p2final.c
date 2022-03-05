748de7d05c49122a0fda2d70b737c0c70722736e
Maneesh1721 <94883199+Maneeshparuchuri@users.noreply.github.com>
2022-03-05 04:41:02 +0000

    p1final.c p2final.c

--- p2final.c
+++ p2final.c
@@ -1,1 +1,28 @@
 #include<stdio.h>
+struct _fraction
+{
+int num,den;
+
+};
+typedef _fraction Fraction
+Fraction input_fraction();
+{
+  fraction f;
+  printf("enter the fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+  }
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+  
+}
+fraction find_
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+{
+
+
+  
+
+  output(f1,f2,f3,smallest);
+  return 0;
+}
